ed9e6d1554a4ecce0b8fa3a2813ea8755d70e7de
matt <metropolitan.matt@gmail.com>
2019-04-13 21:29:17 -0400

    adding initial scripts and sprites

---
+++ Assets/Scripts/PlayerMobility.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMobility : MonoBehaviour
+{
+    public float speed;
+
+    private void FixedUpdate()
+    {
+        var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        Quaternion rot = Quaternion.LookRotation(transform.position - mousePosition, Vector3.forward);
+
+        transform.rotation = rot;
+        transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z);
+        GetComponent<Rigidbody2D>().angularVelocity = 0;
+
+        float input = Input.GetAxis("Vertical");
+        GetComponent<Rigidbody2D>().AddForce(gameObject.transform.up * speed * input);
+        
+    }// 19:42
+}
